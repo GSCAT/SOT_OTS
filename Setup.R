468956ecc2b3547eb8e3d6ca9e2f974f8b97f8da
Kevin Bonds <kevin_bonds@gap.com>
2018-04-13 19:40:26 -0700

    Add else_if to handle when 1st day is Sunday

--- Setup.R
+++ Setup.R
@@ -1,7 +1,7 @@
 # Install missing packages ----
 list.of.packages <- c("dplyr", "readr", "RODBC", "formattable", 
                       "rJava", "rChoiceDialogs", "ggvis", "tidyr", 
-                      "colorspace",  "mosaic", "yaml", "RJDBC", "DBI", "devtools")
+                      "colorspace",  "mosaic", "yaml", "RJDBC", "DBI", "devtools", "lubridate", "glue")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
 
@@ -21,6 +21,7 @@ library(lubridate)
 library(RJDBC)
 library(DBI)
 library(devtools)
+library(glue)
 
 # Start with clean environment ----
 # rm(list = ls())
@@ -42,3 +43,95 @@ Report_period <- function(We, Mo, Yr) {
   fis_yr <<- if(interactive()) as.integer(Yr)
   fis_month <<- as.integer(Mo)
 }
+
+
+fiscal_conv <- function(dt){
+  
+  date_align = week(dt) - 4
+  month_num <- case_when(
+    date_align %in% c(1:4) ~ as.integer(1),
+    date_align %in% c(5:9) ~ as.integer(2),
+    date_align %in% c(10:13) ~ as.integer(3),
+    date_align %in% c(14:17) ~ as.integer(4),
+    date_align %in% c(18:22) ~ as.integer(5),
+    date_align %in% c(23:26) ~ as.integer(6),
+    date_align %in% c(27:30) ~ as.integer(7),
+    date_align %in% c(31:35) ~ as.integer(8),
+    date_align %in% c(36:39) ~ as.integer(9),
+    date_align %in% c(40:43) ~ as.integer(10),
+    date_align %in% c(44:48) ~ as.integer(11),
+    date_align %in% c(49:53) ~ as.integer(12))
+  month_name <- month(month_num, label = TRUE, abbr = FALSE)
+  year_num <- case_when(date_align %in% c(1:4) ~ year(dt) - 1, 
+                        date_align %in% c(5:53) ~ year(dt))
+
+  
+  return(list(fis_month_number = month_num, fis_month_name = month_name, fis_year_num = year_num))
+}
+
+year_52 <- rep(1:53, each = 7, len = 364)
+year_53 <- rep(1:53, each = 7, len = 371)
+
+Fiscal_cal <- data.frame("Fiscal_Date" = seq.Date(from = as.Date("2017-01-29"), to = as.Date("2020-02-01"), by = "1 day"),
+                     "Fiscal_Week" = c(year_53, year_52, year_52))
+
+
+function (x, with_year = FALSE) 
+{
+  m <- month(x)
+  quarters <- rep(1:4, each = 3)
+  q <- quarters[m]
+  if (with_year) 
+    year(x) + q/10
+  else q
+}
+
+
+# If Feb 1st is a Monday, Tuesday, or Wednesday, then first day of FY is last Sunday of January.
+# If Feb 1st is a Thursday, Friday, Saturday, or Sunday, then first day of FY is first Sunday of February.
+
+function (x) 
+{
+  year_52 <- rep(1:53, each = 7, len = 364)
+  year_53 <- rep(1:53, each = 7, len = 371)
+
+  y <- year(x)
+  if (leap_year(x)) 
+    year_53[y]
+  else q
+}
+
+end_date <- "2017-04-01"
+
+fiscal_454 <- function (a_date)
+{
+  y = year(a_date)
+  # cat(y)
+  Feb1st = as.Date(glue("{y}-02-01"))
+  Sunday1 = Feb1st
+  week_day = as.character(wday(Feb1st, label = TRUE, abbr = TRUE))
+  cat(week_day)
+  
+  if (week_day %in% c("Mon", "Tues", "Wed")) {
+    # cat("Is it Wed? ", week_day == c("Wed"))
+     while (week_day != "Sun") {
+       week_day = as.character(wday(Sunday1 - 1, label = TRUE, abbr = TRUE))
+       Sunday1 = Sunday1 - 1
+      cat("First Sunday is ", Sunday1)
+    # last Sunday in January is first day of year
+    }
+   }
+    else if(week_day %in% c("Thu", "Fri", "Sat")){
+  #     # First Sunday in February 
+  while (week_day != "Sun") {
+    week_day = as.character(wday(Sunday1 + 1, label = TRUE, abbr = TRUE))
+    Sunday1 = Sunday1 + 1
+    cat("First Sunday is ", Sunday1)
+  } 
+    }
+  else {}
+  return(Sunday1)
+}
+
+
+
--- parse_update_edw.R
+++ parse_update_edw.R
@@ -9,15 +9,15 @@ if(!"credentials" %in% ls()){
 }
 
 # Create RODBC connection ----
- my_connect <- odbcConnect(dsn= "IP EDWP", uid= credentials$my_uid, pwd= credentials$my_pwd)
+ # my_connect <- odbcConnect(dsn= "IP EDWP", uid= credentials$my_uid, pwd= credentials$my_pwd)
 # sqlTables(my_connect, catalog = "EDWP", tableName  = "tables")
- sqlQuery(my_connect, query = "SELECT  * from dbc.dbcinfo;")
+# sqlQuery(my_connect, query = "SELECT  * from dbc.dbcinfo;")
  
  drv=JDBC("com.teradata.jdbc.TeraDriver","C:\\TeraJDBC__indep_indep.16.10.00.05\\terajdbc4.jar;C:\\TeraJDBC__indep_indep.16.10.00.05\\tdgssconfig.jar")
  my_connect=dbConnect(drv,"jdbc:teradata://10.101.83.123/LOGMECH=LDAP",credentials$my_uid, credentials$my_pwd)
  dbGetQuery(my_connect, statement = "SELECT  * from dbc.dbcinfo;")
  
- my_query <- readLines('test_sql3.sql')
+ # my_query <- readLines('test_sql3.sql')
  
  #my_query <- readLines("https://github.gapinc.com/raw/SRAA/EDW_IUF/master/Create%20SRAA_SAND%20EDW_IUF_YTD%20Prod.sql")
  my_query <- readLines("Create SRAA_SAND EDW_IUF_YTD Prod_test.sql", warn = FALSE)
@@ -26,27 +26,10 @@ if(!"credentials" %in% ls()){
  my_query <- gsub("^\\s+|\\s+$", "", my_query) 
  my_query <- gsub("\t", " ", my_query) 
  my_query <- gsub("\n", " ", my_query) 
- my_query <- gsub(";\\*/", "\\*/;", my_query) 
+# my_query <- gsub(";\\*/", "\\*/;", my_query) 
+ my_query <- gsub("/\\*.*?\\*/", "", my_query) 
  # my_query <- gsub("--", "-- \n", my_query) 
  my_query <- strsplit(my_query, split = ";")
- 
-       # my_query <- paste("BT; \n",
-       #                  "create table SRAA_SAND.test2
-       #                    (my_col1 integer,
-       #                     my_col2 varchar(6));"
-       #                  ,"ET; \n",
-       #                  "BT; \n",   
-       #                     "insert into SRAA_SAND.test2 (my_col1, my_col2)
-       #                     values (6, 'chicks');
-       #                     
-       #                     insert into SRAA_SAND.test2 (my_col1, my_col2)
-       #                     values (1, 'dog');", "BT; \n", sep= " " )
-       # my_query <- paste("BT; \n",
-       #                     "insert into SRAA_SAND.test2 (my_col1, my_col2)
-       #                     values (6, 'chicks');
-       #                     
-       #                     insert into SRAA_SAND.test2 (my_col1, my_col2)
-       #                     values (1, 'dog');", "ET; \n", sep= " " )
  
 
  iterate_sql <- function(list){
@@ -56,6 +39,8 @@ if(!"credentials" %in% ls()){
  }
  
 system.time(lapply(my_query[[1]], FUN = iterate_sql))
+ 
+ dbGetQuery(my_connect, statement = "SELECT data_pulled from SRAA_SAND.EDW_IUF_YTD sample 1;")
  
  # my_query <- as.list(my_query[[1]][-1])
  # dbSendUpdate(my_connect, "DROP TABLE SRAA_SAND.EDW_IUF_YTD;")
