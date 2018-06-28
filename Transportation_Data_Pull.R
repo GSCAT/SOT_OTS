008753e721fb47d3ae13fd18fb3055970e439ccf
Kevin Bonds <kevin_bonds@gap.com>
2018-06-28 14:19:01 -0700

    Clean Transportation_Data_Pull.R

--- Transportation_Data_Pull.R
+++ Transportation_Data_Pull.R
@@ -6,11 +6,11 @@ library(RODBC)
 ####### MUST install Microsoft Access Database Engine for 64 bit if running 64 bit R ##########
 ###############################################################################################
 channel <- odbcDriverConnect("Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=\\\\Americas\\sf\\SF-LOGISTICS_METRICS\\Dashboards\\Databases\\IB Databases\\IB_Daily_2018.accdb")
-Transportation_Daily_IB <- sqlQuery( channel , paste ("select [Purchase Order Number], [PO Type], [Container Number1], [BL/AWB#1], [Vessel], [InDC Calc Method], [INDC], [INDC+2], [Total Shipped Qty] from Daily_Tracker_2_0"))
+Transportation_Daily_IB <- sqlQuery( channel , "select [Purchase Order Number], [PO Type], [Container Number1], [BL/AWB#1], [Vessel], [InDC Calc Method], [INDC], [INDC+2], [Total Shipped Qty] from Daily_Tracker_2_0")
 close(channel)
 
 channel <- odbcDriverConnect("Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=\\\\Americas\\sf\\SF-LOGISTICS_METRICS\\Dashboards\\Databases\\IB Databases\\Monthly\\IB_Monthly_2018.accdb")
-Transportation_Month_IB <- sqlQuery( channel , paste ("select [Purchase Order Number], [PO Type], [Container Number1], [BL/AWB#1], [Vessel], [InDC Calc Method], [INDC], [INDC+2], [Total Shipped Qty] from IB_Summary;"))
+Transportation_Month_IB <- sqlQuery( channel , "select [Purchase Order Number], [PO Type], [Container Number1], [BL/AWB#1], [Vessel], [InDC Calc Method], [INDC], [INDC+2], [Total Shipped Qty] from IB_Summary;")
 close(channel)
 
 Transportation_data_combine <- rbind(Transportation_Daily_IB, Transportation_Month_IB)
